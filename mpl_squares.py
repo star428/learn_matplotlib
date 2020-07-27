3fb39496ce85e420b0c64df393b606dd38069f87
star428 <62631630+star428@users.noreply.github.com>
2020-07-27 22:07:25 +0800

    test matplotlib

---
+++ mpl_squares.py
@@ -0,0 +1,15 @@
+'''
+@Author: star428
+@Date: 2020-07-27 21:57:46
+@LastEditTime: 2020-07-27 22:06:47
+@LastEditors: Please set LastEditors
+@Description: test matplotlib
+@FilePath: \learn_matplotlib\mpl_squares.py
+'''
+
+import matplotlib.pyplot as plt
+
+
+squares = [1, 4, 9, 16, 25]
+plt.plot(squares)
+plt.show()
